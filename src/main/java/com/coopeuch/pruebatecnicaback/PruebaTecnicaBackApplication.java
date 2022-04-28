5a90c6fe2129869b862e58085c606079da3eb9c0
felipe <f>
2022-04-28 19:01:13 -0400

    Initial commit

---
+++ src/main/java/com/coopeuch/pruebatecnicaback/PruebaTecnicaBackApplication.java
@@ -0,0 +1,13 @@
+package com.coopeuch.pruebatecnicaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coopeuch/pruebatecnicaback/PruebaTecnicaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coopeuch.pruebatecnicaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
